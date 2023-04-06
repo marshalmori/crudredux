0b0aaedd53b7852151854dfef00246d14683ed60
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-06 14:19:48 -0300

    Criar o header da aplicação

--- src/App.js
+++ src/App.js
@@ -1,5 +1,12 @@
+import { Fragment } from "react";
+import Header from "./components/Header";
+
 function App() {
-  return <h1>Redux</h1>;
+  return (
+    <Fragment>
+      <Header />
+    </Fragment>
+  );
 }
 
 export default App;

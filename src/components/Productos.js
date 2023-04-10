ea8d04fbdaf084df32c62db917c91577b37b0234
Marshal Mori Cavalheiro <marshalmori@gmail.com>
2023-04-10 14:31:07 -0300

    Mostrar mensagens de erros e o texto carregando a página

--- src/components/Productos.js
+++ src/components/Productos.js
@@ -11,14 +11,23 @@ const Productos = () => {
     //Consultar la API
     const cargarProductos = () => dispatch(obtenerProductosAction());
     cargarProductos();
-  }, []);
+  }, [dispatch]);
 
   // obtener el state
   const productos = useSelector((state) => state.productos.productos);
+  const error = useSelector((state) => state.productos.error);
+  const cargando = useSelector((state) => state.productos.loading);
 
   return (
     <>
       <h2 className="text-center my-5">Listado de Productos</h2>
+      {error ? (
+        <p className="font-weight-bold alert alert-danger text-center mt-4">
+          Hubo un error
+        </p>
+      ) : null}
+
+      {cargando ? <p className="text-center">Cargando...</p> : null}
 
       <table className="table table-striped">
         <thead className="bg-primary table-dark">
